8411764f22989b94161f808d3f1eee0eb73d8dde
Mahan Mehrabi <gnusmhn@gmail.com>
2025-07-17 16:00:01 +0330

    Update App.jsx

--- 001.React Basics/src/App.jsx
+++ 001.React Basics/src/App.jsx
@@ -1,11 +1,15 @@
+// Importing Components from their respective files
+// Paths are relative to the current file (./ means same folder)
 import Header from './Header';
 import Footer from './Footer';
 import Food from './Food';
 
-
+// Main App Component (Functional Component)
 function App() {
   return (
+     // <> </> is a React Fragment (groups elements without adding extra DOM nodes)
     <>
+      {/*Reusable components: */}
       <Header />
       <Food />
       <Food />
@@ -14,4 +18,5 @@ function App() {
   );
 }
 
+// Export App as the default export (so it can be imported elsewhere)
 export default App
