ba30733545d857bb69db4ad7f1194fa99b94ddea
Mahan Mehrabi <gnusmhn@gmail.com>
2025-07-17 16:04:19 +0330

    Add files via upload

---
+++ 002.Card Components/src/App.jsx
@@ -0,0 +1,16 @@
+import Card from "./card";
+
+function App() {
+  return (
+    <>
+      <Card />
+      <Card />
+      <Card />
+      <Card />
+    </>
+  );
+}
+
+export default App
+
+//Card Components 
