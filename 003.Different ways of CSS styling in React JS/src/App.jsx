f0a8612d347a5531951260a66aae62ce9fdf91f9
Mahan Mehrabi <gnusmhn@gmail.com>
2025-07-20 16:56:43 +0330

    Add files via upload

---
+++ 003.Different ways of CSS styling in React JS/src/App.jsx
@@ -0,0 +1,18 @@
+// HOW TO STYLE REACT COMPONENTS WITH CSS
+// (not including external frameworks or preprocessors)
+
+// 1.EXTERNAL
+// 2.MODULES
+// 3.INLINE
+
+import Button from "./Button/Button";
+
+function App() {
+  return (
+
+    <Button />
+
+  );
+}
+
+export default App
