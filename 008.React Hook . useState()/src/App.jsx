444cf8a8325a62684e27c072071701798207115f
Mahan Mehrabi <gnusmhn@gmail.com>
2025-08-26 15:21:55 +0330

    Add files via upload

---
+++ 008.React Hook . useState()/src/App.jsx
@@ -0,0 +1,18 @@
+// React hook = special function that allowsfunctionaal components
+//              to use React features  without writing class components
+//              (useState, useEffect, useContext, useReducer, useCallback and more ...)
+
+// useState = A Reacct hook that allows the creation of stateful variable
+//           AND a setter function to update it's value in the virtual DOM.
+//          [name, setName]
+
+import MyComponent from "./myComponent";
+
+function App() {
+  return (
+    <MyComponent />
+  );
+}
+
+export default App;
+
