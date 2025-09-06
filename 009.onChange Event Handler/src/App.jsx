1019fdf57161b0f3ad93ec856e02150deb343124
Mahan Mehrabi <gnusmhn@gmail.com>
2025-09-06 16:10:19 +0330

    Add files via upload

---
+++ 009.onChange Event Handler/src/App.jsx
@@ -0,0 +1,14 @@
+// onChange event handler
+// onChange = event handler used primarily with form elements 
+//            ex. <input>, <textarea>, <select>, <radio>
+//            Triggers a function every time the value of the input changes 
+
+import MyComponent from "./myComponent";
+
+function App() {
+  return (
+    <MyComponent />
+  );
+}
+
+export default App;
