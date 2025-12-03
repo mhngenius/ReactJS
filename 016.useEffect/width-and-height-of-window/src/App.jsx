92ce7c8264a58c8d46033bbdca8a218bc84ed7b0
Mahan Mehrabi <gnusmhn@gmail.com>
2025-12-03 20:30:59 +0330

    Add files via upload

---
+++ 016.useEffect/width-and-height-of-window/src/App.jsx
@@ -0,0 +1,11 @@
+// width and height of the DOM
+
+import MyComponent from "./MyComponent";
+
+function App() {
+  return (
+    <MyComponent />
+  );
+}
+
+export default App;
---
+++ 016.useEffect/width-and-height-of-window/src/MyComponent.jsx
@@ -0,0 +1,36 @@
+// width and height of the DOM (window)
+
+import React, { useState, useEffect } from 'react';
+
+function MyComponent() {
+
+    const [width, setWidth] = useState(window.innerWidth);
+    const [height, setHeight] = useState(window.innerHeight);
+
+    useEffect(() => {
+        window.addEventListener("resize", handleResize);
+        console.log("EVENT LISTENER ADDED"); // adds twice
+
+        return () => {
+            window.removeEventListener("resize", handleResize);
+            console.log("EVENT LISTENER REMOVED");
+        }
+    }, []);
+
+    useEffect(() => {
+        document.title = `Size: ${width} x ${height}`;
+    }, [width, height])
+
+    function handleResize() {
+        setWidth(window.innerWidth);
+        setHeight(window.innerHeight);
+    }
+
+    return (<>
+        <p>Window width: {width}px</p>
+        <p>Window height: {height}px</p>
+    </>)
+
+}
+
+export default MyComponent;
